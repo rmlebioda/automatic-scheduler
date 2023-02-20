24c56e37fbb87a8e6851c022f98d9cb093a20b49
rmlebioda <lebioda.rafal@gmail.com>
2023-02-20 10:19:25 +0100

    fixes, cleanups

--- PriceCheckWebScrapper/Core/PriceCheckReport.cs
+++
@@ -1,7 +0,0 @@
-namespace PriceCheckWebScrapper.Core;
-
-[Obsolete("Useless???????????????")]
-public readonly record struct PriceCheckReport(IEnumerable<ProductPriceOffersReport> PriceCheckInstances)
-{
-    public static PriceCheckReport Empty => new PriceCheckReport(Enumerable.Empty<ProductPriceOffersReport>());
-}
